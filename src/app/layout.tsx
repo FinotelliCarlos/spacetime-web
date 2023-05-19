7558488614e45f24832440b19252f2e8d77c25ac
finotellicarlos <carlos.finotelli@outlook.com>
2023-05-19 10:11:15 -0300

    refactor: refact and adjust layout routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,16 @@
+import { Blur } from '@/components/blur'
+import { Copyright } from '@/components/copyright'
+import { Hero } from '@/components/hero'
+import { Profile } from '@/components/profile'
+import { SignIn } from '@/components/signin'
+import { Stripes } from '@/components/stripes'
+import {
+  Bai_Jamjuree as BaiJamjuree,
+  Roboto_Flex as Roboto,
+} from 'next/font/google'
+import { cookies } from 'next/headers'
 import { ReactNode } from 'react'
 import './globals.css'
-import {
-  Roboto_Flex as Roboto,
-  Bai_Jamjuree as BaiJamjuree,
-} from 'next/font/google'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -19,12 +26,31 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('spacetime_token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            <Blur />
+
+            <Stripes />
+
+            {!isAuthenticated && <SignIn />}
+            {isAuthenticated && <Profile />}
+
+            <Hero />
+
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,5 @@
-import { cookies } from 'next/headers'
-
-import { Blur } from '@/components/blur'
-import { Copyright } from '@/components/copyright'
 import { EmptyMemories } from '@/components/empty-memories'
-import { Hero } from '@/components/hero'
-import { SignIn } from '@/components/signin'
-import { Stripes } from '@/components/stripes'
-import { Profile } from '@/components/profile'
 
 export default function Home() {
-  const isAuthenticated = cookies().has('spacetime_token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        <Blur />
-
-        <Stripes />
-
-        {!isAuthenticated && <SignIn />}
-        {isAuthenticated && <Profile />}
-
-        <Hero />
-
-        <Copyright />
-      </div>
-
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
